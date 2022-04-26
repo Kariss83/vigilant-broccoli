52708c896fa51deb9fbaba1ceda56ea0bd9db116
kariss83 <vache.romain@gmail.com>
2022-04-26 12:52:50 +0200

    fixed the issue of empty username that prevented log in and user creation by setting username equal to email adress

--- purbeurre/forms.py
+++ purbeurre/forms.py
@@ -1,17 +1,21 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+import uuid
 
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email'}), )	
-    
+
     class Meta:
         model = User
-        fields = ('email', 'password1', 'password2',)
+        fields = ('username', 'email', 'password1', 'password2',)
+        exclude = ('username',)
 
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
+
+        
 
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['placeholder'] = 'Mot de Passe'
--- purbeurre/users/views.py
+++ purbeurre/users/views.py
@@ -4,41 +4,38 @@ from django.contrib import messages
 from purbeurre.forms import SignUpForm
 
 # Create your views here.
-def home(request):
-	return render(request, 'authenticate/home.html', {})
-
 def login_user(request):
 	if request.method == 'POST':
 		email = request.POST['email']
 		password = request.POST['password']
-		user = authenticate(request, email=email, password=password)
+		user = authenticate(request, username=email, email=email, password=password)
 		if user is not None:
 			login(request, user)
-			messages.success(request, ('You Have Been Logged In!'))
-			return redirect('home')
+			messages.success(request, ('Vous êtes connecté(e)!'))
+			return redirect('/')
 
 		else:
-			messages.success(request, ('Error Logging In - Please Try Again...'))
-			return redirect('login')
+			messages.success(request, ('Erreur de connexion - Veuillez reéssayer...'))
+			return redirect('/login')
 	else:
 		return render(request, 'users/login.html', {})
 
 def logout_user(request):
 	logout(request)
-	messages.success(request, ('You Have Been Logged Out...'))
-	return redirect('home')
+	messages.success(request, ('Vous êtes déconnecté(e)...'))
+	return redirect('/')
 
 def register_user(request):
 	if request.method == 'POST':
 		form = SignUpForm(request.POST)
 		if form.is_valid():
 			form.save()
-			username = form.cleaned_data['username']
+			email = form.cleaned_data['email']
 			password = form.cleaned_data['password1']
-			user = authenticate(username=username, password=password)
+			user = authenticate(username=email, email=email, password=password)
 			login(request, user)
 			messages.success(request, ('Vous êtes enregistré(e)...'))
-			return redirect('home')
+			return redirect('/')
 	else:
 		form = SignUpForm()
 	
